246fd9f833e7b1ca17c15ed17c4c523d297a0b73
Maklolaj <m.zelek@op.pl>
2022-10-06 21:52:30 +0200

    add merge file model

---
+++ Models/MergeFilesModel.cs
@@ -0,0 +1,16 @@
+using Microsoft.AspNetCore.Http;
+using PDFerterDesktopNet.Validators;
+
+namespace PDFerterDesktopNet.Models
+{
+    public class MergeFilesModel
+    {
+        [MaxFileSize(5 * 1024 * 1024)]
+        [AllowedExtensions(new string[] { ".pdf" })]
+        public IFormFile PdfFileOne { get; set; }
+
+        [MaxFileSize(5 * 1024 * 1024)]
+        [AllowedExtensions(new string[] { ".pdf" })]
+        public IFormFile PdfFileTwo { get; set; }
+    }
+}
